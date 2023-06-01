a4b362351345d0086cf4f33028979b1b81a65f27
navajyothdc <123870696+navajyothdc@users.noreply.github.com>
2023-06-01 21:07:26 +0530

    Time: 602 ms (5.45%), Space: 44.8 MB (8.16%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,17 @@
+class Solution { 
+    public int singleNumber(int[] numbers) {
+    int len = numbers.length;
+    boolean isTwice = false; 
+    for (int i=0; i < len; i++) { 
+       isTwice = false;
+       for (int j=0; j<len; j++) {
+         if (isTwice) break;
+         if (i != j)
+            if(numbers[i] == numbers[j]) 
+                isTwice = true;
+       }         
+       if(!isTwice) return numbers[i];
+     }
+     return numbers[len - 1];
+  }
+}
