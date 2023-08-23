a0b81617d057e01908a6dcd40b87170e577234d5
Navajyoth D C <123870696+navajyothdc@users.noreply.github.com>
2023-08-24 00:01:36 +0530

    Time: 0 ms (100%), Space: 40 MB (99.39%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -18,12 +18,35 @@ class Solution {
         return symmetric(root.left,root.right);
     }
     public boolean symmetric(TreeNode root1, TreeNode root2){
-        if(root1 == null && root2 == null) return true;
-        else if(root1 == null && root2 != null) return false;
-        else if(root1 != null && root2 == null) return false;
-        boolean t1 = root1.val==root2.val?true:false;
-        boolean t2 = symmetric(root1.left, root2.right);
-        boolean t3 = symmetric(root1.right, root2.left);
-        return t1 & t2 & t3;
+      boolean k = false;
+      if(root1 == null || root2 == null){
+        if(root1 == root2){
+          return true;
+        } else {
+          return false;
+        }
+      }else{
+        if(root1.val == root2.val){
+          k = true;
+        }
+      }
+      
+      return k && symmetric(root1.left, root2.right) && symmetric(root1.right, root2.left);
     }
 }
+
+
+
+
+
+
+
+
+
+//   if(root1 == null && root2 == null) return true;
+//         else if(root1 == null && root2 != null) return false;
+//         else if(root1 != null && root2 == null) return false;
+//         boolean t1 = root1.val==root2.val?true:false;
+//         boolean t2 = symmetric(root1.left, root2.right);
+//         boolean t3 = symmetric(root1.right, root2.left);
+//         return t1 & t2 & t3;
