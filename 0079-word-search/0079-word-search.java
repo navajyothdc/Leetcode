7798bef8dfd23f72ea7a207634660ee71397a33d
Navajyoth D C <123870696+navajyothdc@users.noreply.github.com>
2023-09-12 23:58:17 +0530

    Time: 1626 ms (5%), Space: 44.1 MB (9.9%) - LeetHub

---
+++ 0079-word-search/0079-word-search.java
@@ -0,0 +1,32 @@
+class Solution {
+    boolean val = false;
+    char[][] test;
+    public boolean exist(char[][] board, String word) {
+        int row = board.length;
+        int col = board[0].length;
+        test = board;
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                if(board[i][j] == word.charAt(0)){
+                    dfs(board, row, col, i, j, word, "");
+                }
+            }
+        }
+        return val;
+    }
+    public void dfs(char[][] grid, int row, int col, int i, int j, String word, String ans){
+        if(i<0 || j<0 || i>=row || j>=col || !word.contains(ans + Character.toString(grid[i][j]))){
+            return;
+        }
+        ans = ans + Character.toString(grid[i][j]);
+        char c = grid[i][j];
+        grid[i][j] = ' ';
+        if(ans.equals(word))
+            val = true;
+        dfs(grid, row, col, i+1, j, word, ans);
+        dfs(grid, row, col, i-1, j, word, ans);
+        dfs(grid, row, col, i, j+1, word, ans);
+        dfs(grid, row, col, i, j-1, word, ans);
+        grid[i][j] = c;
+    }
+}
